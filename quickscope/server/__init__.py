3452e67bafbd175c3cea42616aae28aa1ea8e51b
nicklambourne <ndl93@live.com>
2020-11-17 22:14:30 +1000

    added some tests

--- quickscope/server/__init__.py
+++ quickscope/server/__init__.py
@@ -4,8 +4,8 @@ from flask_cors import CORS
 from .config import Config
 
 
-static_path = str(Path("quickscope/gui/dist/").absolute())
 
+static_path = str(Path("quickscope/gui/dist/").absolute())
 
 app = Flask(__name__, static_folder=static_path,
             template_folder=static_path,
@@ -13,6 +13,5 @@ app = Flask(__name__, static_folder=static_path,
 app.config.from_object(Config)
 CORS(app, expose_headers="Access-Control-Allow-Origin")
 app.logger.info(f"Static and template directory: {static_path}")
-
 
 from quickscope.server import routes
--- test/test_sample.py
+++ test/test_sample.py
@@ -1,4 +1,50 @@
+import io
+from pathlib import Path
+from shutil import rmtree
+
+import pytest
+
+from quickscope.server import app
 
 
 def test_sample() -> None:
     assert 1 + 1 == 2
+
+
+@pytest.fixture(scope="module")
+def test_client():
+    testing_client = app.test_client()
+    context = app.app_context()
+    context.push()
+
+    yield testing_client
+
+    context.pop()
+
+
+def test_home_page(test_client):
+    response = test_client.get("/")
+    assert response.status_code == 200
+    assert b"<div id=\"root\"></div>" in response.data
+
+
+def test_upload(test_client):
+    response = test_client.post("/upload/linter_config",
+                                data={
+                                    "session": "57c9d312-4aed-43f3-a36c-aa5eb3d1c50b",
+                                    "engine": "JavaEngine",
+                                    "assignment_id": "a1",
+                                    "course": "CSSE2002",
+                                    "component": "linter_config",
+                                    "file": (io.BytesIO(b"abcdef"), "checkstyle.xml")
+                                },
+                                follow_redirects=True,
+                                content_type='multipart/form-data'
+                                )
+
+    uploaded = Path("state/57c9d312-4aed-43f3-a36c-aa5eb3d1c50b") / "checkstyle.xml"
+
+    assert response.status_code == 200
+    assert uploaded.exists()
+
+    rmtree(str(uploaded.parent.parent), ignore_errors=True)
