7f36121fcf5828dc473a816c676b156213bc5106
nicklambourne <ndl93@live.com>
2020-11-17 17:28:07 +1000

    bundle comments

--- quickscope/server/bundle.py
+++ quickscope/server/bundle.py
@@ -29,37 +29,67 @@ def get_chalkbox(version: str, bundle_directory: Path) -> Path:
 
 
 def produce_lib_directory(lib_directory: Path, bundle_directory: Path) -> None:
+    """
+    JavaEngine specific. Gathers the lib file containing JAR dependencies and copies it to the
+    temporary bundle directory.
+    :param lib_directory: the lib directory with JAR dependencies uploaded by the user
+    :param bundle_directory: the temporary directory where the bundle is being created
+    """
     if lib_directory.exists():
         copytree(f"{lib_directory}", f"{bundle_directory / 'lib'}")
     else:
         lib_directory.mkdir(parents=True)
 
 
-def produce_resources_directory(lib_directory: Path, bundle_directory: Path) -> None:
-    if lib_directory.exists():
-        copytree(f"{lib_directory}", f"{bundle_directory / 'resources'}")
+def produce_resources_directory(resources_directory: Path, bundle_directory: Path) -> None:
+    """
+    JavaEngine specific. Gathers the static resources directory and files uploaded by the user and
+    copies them to the temporary bundle directory.
+    :param resources_directory: the static resources directory populated by the user's uploads
+    :param bundle_directory: the temporary directory where the bundle is being created
+    """
+    if resources_directory.exists():
+        copytree(f"{resources_directory}", f"{bundle_directory / 'resources'}")
     else:
-        lib_directory.mkdir(parents=True)
+        resources_directory.mkdir(parents=True)
 
 
-def produce_solution_directory(solution: Path, bundle_directory: Path) -> None:
+def produce_solution_directory(solution_directory: Path, bundle_directory: Path) -> None:
+    """
+    Copies the solution directory uploaded by the user to the temporary bundle directory
+    :param solution_directory: the directory containing the correct and faulty solutions uploaded
+    by the user
+    :param bundle_directory: the temporary directory where the bundle is being created
+    """
     solutions_directory = bundle_directory.joinpath("solutions")
     solutions_directory.unlink(missing_ok=True)
-    if solution.exists():
-        copytree(f"{solution}", f"{solutions_directory}")
+    if solution_directory.exists():
+        copytree(f"{solution_directory}", f"{solutions_directory}")
     else:
-        solution.mkdir(parents=True)
+        solution_directory.mkdir(parents=True)
 
 
-def get_dependencies(dependency_path: Path) -> List[str]:
+def get_dependencies(dependency_directory: Path) -> List[str]:
+    """
+    Creates a list of dependencies based on the contents of the dependency directory.
+    :param dependency_directory: where the dependencies uploaded by the user are stored
+    :return: a list of the dependencies' paths (including the dependency directory)
+    """
     dependencies = []
-    for path in dependency_path.iterdir():
+    for path in dependency_directory.iterdir():
         if path.is_file():
             dependencies.append(f"{Path(path.parent.name) / path.name}")
     return dependencies
 
 
 def reformat_test_classes(config: Dict[str, Any], session_directory: Path) -> None:
+    """
+    JavaEngine specific. Transforms the assessable test classes listed in the config from Java
+    import style (e.g. chalkbox.import.style) to path style (e.g. chalkbox/import/style.java).
+    :param config: the config dictionary containing the test classes to update
+    :param session_directory: the directory associated with the user's session where the user's
+    uploads are stored
+    """
     test_directory: Path = session_directory / "solutions/correct/test"
     test_classes = config.get("junit").get("assessableTestClasses")
     java_paths = []
@@ -76,6 +106,14 @@ def reformat_test_classes(config: Dict[str, Any], session_directory: Path) -> No
 
 
 def produce_config_file(form: Dict[str, Any], bundle_directory: Path) -> None:
+    """
+    Takes the form from the React front-end and uses it, in combination with the engine default
+    settings, to populate the configuration and write it to the config.yaml file in the bundle
+    directory.
+    :param form: the immutable form dictionary from the request object populated by the React
+    front-end
+    :param bundle_directory: the temporary directory where the bundle is being created
+    """
     engine = form.get('engine')
     engine_yaml = {"engine": f"chalkbox.engines.{engine}"}
 
@@ -104,6 +142,13 @@ def produce_config_file(form: Dict[str, Any], bundle_directory: Path) -> None:
 
 
 def produce_setup_script(setup_calls: str, bundle_directory: Path) -> None:
+    """
+    Creates the setup.sh script - required by Gradescope to prepare the Ubuntu environment - and
+    places it in the bundle.
+    :param setup_calls: the calls made to e.g. install packages, set the PATH etc. These come from
+    .templates.py
+    :param bundle_directory: the temporary directory where the bundle is being created
+    """
     file_loader = FileSystemLoader("quickscope/templates")
     environment = Environment(loader=file_loader)
     setup_template = environment.get_template("setup.sh")
@@ -113,6 +158,12 @@ def produce_setup_script(setup_calls: str, bundle_directory: Path) -> None:
 
 
 def produce_run_script(run_call: str, bundle_directory: Path = None) -> None:
+    """
+    Creates the run.sh script in the root of the bundle that is used by Gradescope to start the
+    autograding process.
+    :param run_call: typically the call to start ChalkBox with whatever arguments are required
+    :param bundle_directory: the temporary directory where the bundle is being created
+    """
     file_loader = FileSystemLoader("quickscope/templates")
     environment = Environment(loader=file_loader)
     run_template = environment.get_template("run_autograder")
@@ -122,10 +173,21 @@ def produce_run_script(run_call: str, bundle_directory: Path = None) -> None:
 
 
 def produce_included_directory(source: Path, bundle_directory: Path) -> None:
+    """
+    PythonEngine specific. Copies the entire 'included' directory with its uploaded components into
+    the root of the bundle.
+    :param source: the original location of the included directory as uploaded by the user
+    :param bundle_directory: the temporary directory where the bundle is being created
+    """
     copytree(f"{source}", f"{bundle_directory}")
 
 
 def copy_testrunner(bundle_directory: Path) -> None:
+    """
+    PythonEngine specific. Copies the testrunner.py testing utility package from ../templates into
+    the included directory in the bundle.
+    :param bundle_directory: the temporary directory where the bundle is being created
+    """
     testrunner = Path("quickscope/templates/testrunner.py")
     if testrunner.exists():
         copyfile(testrunner, bundle_directory / "included/testrunner.py")
@@ -134,6 +196,12 @@ def copy_testrunner(bundle_directory: Path) -> None:
 
 
 def produce_bundle(config: Dict[str, Any]) -> str:
+    """
+    Performs bundle construction from the various elements based on the Engine and configuration
+    specified.
+    :param config: the configuration dictionary as prepared by .templates.populate_config
+    :return: the path to the zipped bundle
+    """
     bundle_directory = Path(mkdtemp()) / "autograder"
     zip_path = f"{bundle_directory}"
     Path.mkdir(bundle_directory)
